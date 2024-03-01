67d39fb8d81a6dd7d8cb524e0048528171e01198
hlkgt <haihaioell@gmail.com>
2024-03-02 01:37:44 +0700

    feat: create contact layout

---
+++ src/layout/Contact.jsx
@@ -0,0 +1,65 @@
+import React from "react"
+import LinkIcon from "../components/LinkIcon"
+import emailIcon from "../assets/icon/gmail_icon.svg"
+import githubIcon from "../assets/icon/github_icon.svg"
+import linkedinIcon from "../assets/icon/linkedin_icon.svg"
+import instagramIcon from "../assets/icon/instagram_icon.svg"
+import telegramIcon from "../assets/icon/telegram_icon.svg"
+import whatsappIcon from "../assets/icon/whatsapp_icon.svg"
+
+const medias = [
+  {
+    path: "/",
+    icon: emailIcon,
+  },
+  {
+    path: "/",
+    icon: githubIcon,
+  },
+  {
+    path: "/",
+    icon: linkedinIcon,
+  },
+  {
+    path: "/",
+    icon: instagramIcon,
+  },
+  {
+    path: "/",
+    icon: whatsappIcon,
+  },
+  {
+    path: "/",
+    icon: telegramIcon,
+  },
+]
+
+const Contact = () => {
+  return (
+    <section id="contact" className="bg-primary px-40 pt-20 pb-c120">
+      <h1 className="text-6xl text-center text-secondary mb-9">
+        <span className="font-bold">Cari tau lebih banyak tentang saya</span>
+        <br />
+        melalui sosial media
+      </h1>
+      <div className="flex justify-center gap-4">
+        {medias.map((media, i) => {
+          return (
+            <LinkIcon
+              key={i}
+              path={media.path}
+              icon={media.icon}
+              target="_blank"
+              customWidth="w-c60"
+              customHeight="h-c60"
+              widthIcon="w-c35"
+              heightIcon="h-c35"
+            />
+          )
+        })}
+      </div>
+    </section>
+  )
+}
+
+export default Contact
--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -5,7 +5,9 @@ import downloadIcon from "../assets/icon/download_icon.svg"
 
 const Home = () => {
   return (
-    <div className="w-full min-h-dvh bg-primary px-40 flex justify-center items-center flex-col gap-8 capitalize text-6xl text-center text-secondary">
+    <section
+      id="home"
+      className="w-full min-h-dvh bg-primary px-40 flex justify-center items-center flex-col gap-8 capitalize text-6xl text-center text-secondary">
       <p>
         hi, saya <span className="font-bold">leo Marselio</span>
         <br />
@@ -21,7 +23,7 @@ const Home = () => {
           target="blank"
         />
       </div>
-    </div>
+    </section>
   )
 }
 
